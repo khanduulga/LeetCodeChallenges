e2220a358aa2aeace3aabc13bd034f4445d43538
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2023-03-16 19:33:50 -0700

    fixed lining

--- dailyChallenges/implementTrie.js
+++ dailyChallenges/implementTrie.js
@@ -1,39 +1,3 @@
-// var Trie = function() {
-//   var trie = []
-// };
-
-// /** 
-// * @param {string} word
-// * @return {void}
-// */
-// Trie.prototype.insert = function(word) {
-//   trie.push(word)
-// };
-
-// /** 
-// * @param {string} word
-// * @return {boolean}
-// */
-// Trie.prototype.search = function(word) {
-//   trie.find(curr => curr == word)? true : false  
-// };
-
-// /** 
-// * @param {string} prefix
-// * @return {boolean}
-// */
-// Trie.prototype.startsWith = function(prefix) {
-
-// };
-
-// /** 
-// * Your Trie object will be instantiated and called as such:
-// * var obj = new Trie()
-// * obj.insert(word)
-// * var param_2 = obj.search(word)
-// * var param_3 = obj.startsWith(prefix)
-// */
-
 class Trie {
   constructor(){
     this.trie = []
@@ -70,3 +34,41 @@ trie.search("app");     // return False
 trie.startsWith("app"); // return True
 trie.insert("app");
 trie.search("app");     // return True
+
+
+
+// var Trie = function() {
+//   var trie = []
+// };
+
+// /** 
+// * @param {string} word
+// * @return {void}
+// */
+// Trie.prototype.insert = function(word) {
+//   trie.push(word)
+// };
+
+// /** 
+// * @param {string} word
+// * @return {boolean}
+// */
+// Trie.prototype.search = function(word) {
+//   trie.find(curr => curr == word)? true : false  
+// };
+
+// /** 
+// * @param {string} prefix
+// * @return {boolean}
+// */
+// Trie.prototype.startsWith = function(prefix) {
+
+// };
+
+// /** 
+// * Your Trie object will be instantiated and called as such:
+// * var obj = new Trie()
+// * obj.insert(word)
+// * var param_2 = obj.search(word)
+// * var param_3 = obj.startsWith(prefix)
+// */
