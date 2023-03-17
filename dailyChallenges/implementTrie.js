0aba147e0db06810c41dc0c29031dd79028e8e79
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2023-03-16 19:31:32 -0700

    worked with Class constructor and methods to implement a Trie

---
+++ dailyChallenges/implementTrie.js
@@ -0,0 +1,72 @@
+// var Trie = function() {
+//   var trie = []
+// };
+
+// /** 
+// * @param {string} word
+// * @return {void}
+// */
+// Trie.prototype.insert = function(word) {
+//   trie.push(word)
+// };
+
+// /** 
+// * @param {string} word
+// * @return {boolean}
+// */
+// Trie.prototype.search = function(word) {
+//   trie.find(curr => curr == word)? true : false  
+// };
+
+// /** 
+// * @param {string} prefix
+// * @return {boolean}
+// */
+// Trie.prototype.startsWith = function(prefix) {
+
+// };
+
+// /** 
+// * Your Trie object will be instantiated and called as such:
+// * var obj = new Trie()
+// * obj.insert(word)
+// * var param_2 = obj.search(word)
+// * var param_3 = obj.startsWith(prefix)
+// */
+
+class Trie {
+  constructor(){
+    this.trie = []
+  }
+
+  insert(word) {
+    return this.search(word)? this.trie.push(word) : null
+  }
+
+  search(word) {
+    return this.trie.find(curr => curr == word)? true : false
+  }
+
+  startsWith(prefix) {
+    var i = 0
+    var length = this.trie.length
+
+    while(i < length) {
+      var preWord = this.trie[i].slice(0,prefix.length)
+      if (preWord == prefix) {
+        return true
+      }
+      i++
+    }
+
+    return false
+  }
+}
+
+const trie = new Trie();
+trie.insert("apple");
+trie.search("apple");   // return True
+trie.search("app");     // return False
+trie.startsWith("app"); // return True
+trie.insert("app");
+trie.search("app");     // return True
--- dailyChallenges/mergeKsortedList.js
+++ dailyChallenges/mergeKsortedList.js
@@ -10,8 +10,9 @@
  * @return {ListNode}
  */
 var mergeKLists = function(lists) {
-  let oneBigSortedList = lists.join().split(',').sort(function compareFn(a, b) {return a - b}).reduceRight((next, currValue) => ({currValue, next}))
+  let oneBigSortedList = lists.join().split(',').map(x => Number(x)).sort(function compareFn(a, b) {return a - b})
   return oneBigSortedList
 };
 
 console.log(mergeKLists([[1,4,5],[1,3,4],[2,6]]))
+console.log(mergeKLists([[1,4,5],[1,3,4],[2,6]]).map(x => typeof x), typeof mergeKLists([[1,4,5],[1,3,4],[2,6]]))
